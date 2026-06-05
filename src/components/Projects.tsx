7c1b490eca3634da91c2f1b75e1015ed36f787dd
Foxy-Ze <ayushzunjare9@gmail.com>
2026-06-06 00:50:30 +0530

    Initial commit: Flowforge architecture complete

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,5 +1,5 @@
 import { motion, AnimatePresence } from "motion/react";
-import React, { useState, useEffect, useRef, memo } from "react";
+import { useState, useEffect, useRef, memo } from "react";
 import { ExternalLink, X, ArrowRight, Image as ImageIcon } from "lucide-react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
